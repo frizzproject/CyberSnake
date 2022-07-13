da717752f927454788485056465ce80ce0b3be6c
Maxim <mbf.programmer@gmail.com>
2022-07-13 19:35:46 +0300

    Fix collisions for snake 1.1

--- src/js/Snake.js
+++ src/js/Snake.js
@@ -111,13 +111,13 @@ export default class Snake {
         if (this.x < 0) {
             this.x = width - this.config.sizeCell;
         } else if (this.x >= width) {
-            this.x = -this.config.sizeCell;
+            this.x = 0;
         }
     
         if (this.y < 0) {
             this.y = height - this.config.sizeCell;
         } else if (this.y >= height) {
-            this.y = -this.config.sizeCell;
+            this.y = 0;
         }
     }
 
